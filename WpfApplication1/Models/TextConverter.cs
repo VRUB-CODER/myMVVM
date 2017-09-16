5cdf32ad0f5bd6f054b44e772c1901c27709d8bb
VRUB-CODER <vrubelch@gmail.com>
2017-09-17 00:42:59 +0300

    Add files via upload

---
+++ WpfApplication1/Models/TextConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Models
+{
+    class TextConverter
+    {
+        private readonly Func<string, string> _convertion;
+
+        public TextConverter(Func<string, string> convertion)
+        {
+            _convertion = convertion;
+        }
+
+        public string ConvertText(string inputText)
+        {
+            return _convertion(inputText);
+        }
+
+        public bool IsValid()
+        {
+            if (_convertion == null)
+                return false;
+            else return true;
+        }
+    }
+}
---
+++ WpfApplication1/ViewModels/DelCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1.ViewModels
+{
+    class DelCommand : ICommand
+    {
+
+        private readonly Action _action;
+        private Func<bool> WhentoExecute;
+
+        public DelCommand(Action action, Func<bool> When)
+        {
+            _action = action;
+            WhentoExecute = When;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return WhentoExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
